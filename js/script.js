ad4447ac16c048193e960063f8a0b4b7394af4de
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-15 20:31:39 -0400

    fixed home page bug

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ function handleSearchFormSubmit(event) {
     var searchInputVal = document.querySelector('#search-input').value;
 
     if (!searchInputVal) {
-        console.error('You have not searched for your city!');
+        alert('You have not searched for your city!');
         return;
     }
-    var queryString = './weather?q=' + searchInputVal + '&appid' + apiKey
+    var queryString = './weather-results.html?q=' + searchInputVal + '&appid=' + apiKey;
 
     location.assign(queryString);
 }
