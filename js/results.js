d214bd7c3b9e8cf6babad56f2b9e71538c240765
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-15 23:46:33 -0400

    added javascript functionality to results html page

--- js/results.js
+++ js/results.js
@@ -2,117 +2,134 @@ var resultTextEl = document.querySelector('#result-text');
 var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form');
 
-function getParams () {
-    var searchParamsArr = document.location.search.split('&');
+function getParams() {
+  var searchParamsArr = document.location.search.split('&');
 
-    var query = searchParamsArr.find(function(param) {
-        return param.includes('?q=')
-    });
-    var key = searchParamsArr.find(function(param) {
-        return param.includes('appid=');
-    });
-    if(!query || key) {
-        console.log('You are missing a parameter!')
-        return;
-    }
-    var queryEl = query.split('=').pop();
-    var appidEl = query.split('=').pop();
+  var query = searchParamsArr[0].split('=').pop();
+  var appid = searchParamsArr[1].split('=').pop();
 
-    searchApi(queryEl, appidEl);
+  if (!query || !appid) {
+    console.log('You are missing a parameter!');
+    return;
+  }
+  searchApi(query, appid);
 }
 
-function printResults(resultObj) {
-    console.log(resultObj);
+function printResult(resultObj) {
+  console.log(resultObj);
 
-    var resultCard = document.createElement('div');
-    resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
-    var resultBody = document.createElement('div');
-    resultBody.classList.add('card-body');
-    resultCard.append(resultBody);
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
 
-    var titleEl = document.createElement('h3');
-    titleEl.textContent = resultObj.title;
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = resultObj.name;
 
-    var bodyContentEl = document.createElement('p');
-    bodyContentEl.innerHTML =
-      '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-  
-    if (resultObj.subject) {
-      bodyContentEl.innerHTML +=
-        '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-    } else {
-      bodyContentEl.innerHTML +=
-        '<strong>Subjects:</strong> No subject for this entry.';
-    }
-  
-    if (resultObj.description) {
-      bodyContentEl.innerHTML +=
-        '<strong>Description:</strong> ' + resultObj.description[0];
-    } else {
-      bodyContentEl.innerHTML +=
-        '<strong>Description:</strong>  No description for this entry.';
-    }
-    var linkButtonEl = document.createElement('a');
-  linkButtonEl.textContent = 'Read More';
-  linkButtonEl.setAttribute('href', resultObj.url);
-  linkButtonEl.classList.add('btn', 'btn-dark');
+  var bodyContentEl = document.createElement('ul');
+  bodyContentEl.innerHTML =
+    '<li>Date:</li>' + new Date(resultObj.dt * 1000) + '<br/>' +
+    '<li>Weather:</li> ' + resultObj.weather[0].description + '<br/>' +
+    '<li>Temperature:</li> ' + resultObj.main.temp + '&#8451;<br/>' +
+    '<li>Humidity:</li> ' + resultObj.main.humidity + '%<br/>' +
+    '<li>Wind Speed:</li> ' + resultObj.wind.speed + ' m/s<br/>';
 
-  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
+  resultBody.append(titleEl, bodyContentEl);
   resultContentEl.append(resultCard);
 }
 
-function searchApi(queryEl, appidEl) {
-    var localQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + queryEl + '&appid=' + appidEl;
+function displayFiveDayForecast(forecastObj) {
+  console.log(forecastObj);
 
-    if (queryEl) {
-        localQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?' + queryEl;
-    }
+  var forecastContainer = document.createElement('div');
+  forecastContainer.classList.add('forecast-container');
 
-    localQueryUrl = localQueryUrl + '&appid=' + appidEl;
+  for(var i = 0; i < forecastObj.list.length; i++) {
+    var forecastCard = document.createElement('div');
+    forecastCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
-   fetch(localQueryUrl)
-   .then(function (response) {
-    if(!response.ok) {
+    var forecastBody = document.createElement('div');
+    forecastBody.classList.add('card-body');
+    forecastCard.append(forecastBody);
+
+    var forecastContentEl = document.createElement('ul');
+    forecastContentEl.innerHTML =
+      '<li>Date:</li>' + new Date(forecastObj.list[i].dt * 1000) + '<br/>' +
+      '<li>Weather:</li> ' + forecastObj.list[i].weather[0].description + '<br/>' +
+      '<li>Temperature:</li> ' + forecastObj.list[i].main.temp + '&#8451;<br/>' +
+      '<li>Humidity:</li> ' + forecastObj.list[i].main.humidity + '%<br/>' +
+      '<li>Wind Speed:</li> ' + forecastObj.list[i].wind.speed + ' m/s<br/>';
+    
+    forecastBody.append(forecastContentEl);
+    forecastContainer.append(forecastCard);
+  }
+  resultContentEl.append(forecastContainer);
+}
+
+function searchApi(query, appid) {
+  var localQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + query;
+
+  if (!localQueryUrl.includes('appid=')) {
+    localQueryUrl = localQueryUrl + '&appid=' + appid;
+  }
+
+
+  fetch(localQueryUrl)
+    .then(function (response) {
+      if (!response.ok) {
         throw response.json();
-    }
-    return response.json();
-   })
-   .then(function (locRes) {
-    resultTextEl.textContent = locRes.search.queryEl;
+      }
 
-    console.log(locRes);
+      return response.json();
+    })
+    .then(function (locRes) {
+      resultTextEl.textContent = query;
 
-    if(!locRes.results.length) {
-        console.log('No results found :( ');
-        resultContentEl.innerHTML = '<h3>No results found, search again!<h3>';
-    } else {
+      if (locRes.cod !== 200) {
+        console.log('No results found!');
+        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+      } else {
         resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.results.length; i++) {
-            printResults(locRes.resultso[i]);
-        }
-    }
-   })
-   .catch(function (error) {
-    console.log(error);
-   });
+        printResult(locRes);
+
+        var fiveDayForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + query + '&appid=' + appid;
+
+
+        fetch(fiveDayForecastUrl)
+          .then(function (response) {
+            if (!response.ok) {
+              throw response.json();
+            }
+
+            return response.json();
+          })
+          .then(function (forecastRes) {
+            console.log(forecastRes);
+            displayFiveDayForecast(forecastRes);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
-  
-    var searchInputVal = document.querySelector('#search-input').value;
-    var formatInputVal = document.querySelector('#format-input').value;
-  
-    if (!searchInputVal) {
-      console.error('You need a search input value!');
-      return;
-    }
-  
-    searchApi(searchInputVal, formatInputVal);
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
   }
-  
-  searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-  
-  getParams();
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+getParams();
